495d0f373907522a24025660073bd9952f7191e7
Soslan Doev <soslan2002osetia@mail.ru>
2022-10-24 19:50:57 +0300

    dsagsadg

--- js/script.js
+++ js/script.js
@@ -2,20 +2,28 @@ const
     // cursor = document.getElementById('cursor'),
     time = document.getElementById('time')
 let 
+    layers = document.getElementById('layers'),
     sticky__items = document.getElementById('sticky__items'),
-    sticky__parent = document.getElementById('sticky__parent')
+    sticky__inner = document.getElementById('sticky__inner'),
+    layers__nord1 = document.getElementById('layers__nord1'),
+    layers__nord2 = document.getElementById('layers__nord2')
     scroll__width = sticky__items.scrollWidth, // Получение ширины содержимого   
-    vertical__scroll__height = sticky__parent.getBoundingClientRect()/* Получение координатов*/.height - sticky__items.getBoundingClientRect().height
-setInterval(() => {
+    vertical__scroll__height = sticky__inner.getBoundingClientRect()/* Получение координатов*/.height - sticky__items.getBoundingClientRect().height
+setInterval(() => { 
     time.innerHTML = new Date().toLocaleTimeString()
 }, 1000)
+layers.addEventListener('mousemove', function(event) {
+    const half__width = layers.getBoundingClientRect().width
+    layers__nord1.style.cssText += `--rotate__x: ${(event.offsetX / half__width)}%`
+    layers__nord2.style.cssText += `--rotate__x: ${(event.offsetX / -half__width)}%`
+    console.log(`nord1: --rotate__x: ${(event.offsetX / half__width)}%`)
+    console.log(`nord2: --rotate__x: ${(event.offsetX / half__width)}%`)
+})
 window.addEventListener('scroll', (e) => {
     document.body.style.cssText = `--scrollTop: ${this.scrollY}px`
     let sticky__position = sticky__items.getBoundingClientRect().top
-    if (sticky__position > 1) {
-        return
-    } else {
-        let scrolled = sticky__parent.getBoundingClientRect().top
-        sticky__items.scrollLeft = (scroll__width / vertical__scroll__height) * (-scrolled) * 0.85
+    if (sticky__position > 1) { return } else {
+        let scrolled = sticky__inner.getBoundingClientRect().top
+        sticky__items.scrollLeft = (scroll__width / vertical__scroll__height) * (-scrolled) / 0.85 
     }
 })
