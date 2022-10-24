0b56d1fa00c3bf267f06325783877ee403ce3e12
Soslan Doev <soslan2002osetia@mail.ru>
2022-10-24 19:52:04 +0300

    asgdsadg

--- js/script.js
+++ js/script.js
@@ -5,20 +5,11 @@ let
     layers = document.getElementById('layers'),
     sticky__items = document.getElementById('sticky__items'),
     sticky__inner = document.getElementById('sticky__inner'),
-    layers__nord1 = document.getElementById('layers__nord1'),
-    layers__nord2 = document.getElementById('layers__nord2')
     scroll__width = sticky__items.scrollWidth, // Получение ширины содержимого   
     vertical__scroll__height = sticky__inner.getBoundingClientRect()/* Получение координатов*/.height - sticky__items.getBoundingClientRect().height
 setInterval(() => { 
     time.innerHTML = new Date().toLocaleTimeString()
 }, 1000)
-layers.addEventListener('mousemove', function(event) {
-    const half__width = layers.getBoundingClientRect().width
-    layers__nord1.style.cssText += `--rotate__x: ${(event.offsetX / half__width)}%`
-    layers__nord2.style.cssText += `--rotate__x: ${(event.offsetX / -half__width)}%`
-    console.log(`nord1: --rotate__x: ${(event.offsetX / half__width)}%`)
-    console.log(`nord2: --rotate__x: ${(event.offsetX / half__width)}%`)
-})
 window.addEventListener('scroll', (e) => {
     document.body.style.cssText = `--scrollTop: ${this.scrollY}px`
     let sticky__position = sticky__items.getBoundingClientRect().top
