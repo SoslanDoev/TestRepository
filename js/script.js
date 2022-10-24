bd78d9801c3ab01a18abf0170f75e6ad072bc769
Soslan Doev <soslan2002osetia@mail.ru>
2022-10-24 23:07:54 +0300

    dsagsagkj

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
-    // cursor = document.getElementById('cursor'),
 let 
     layers = document.getElementById('layers'),
     sticky__items = document.getElementById('sticky__items'),
     sticky__inner = document.getElementById('sticky__inner'),
     scroll__width = sticky__items.scrollWidth, // Получение ширины содержимого   
-    vertical__scroll__height = sticky__inner.getBoundingClientRect()/* Получение координатов*/.height - sticky__items.getBoundingClientRect().height
+    vertical__scroll__height = sticky__inner.getBoundingClientRect()/* Получение координатов*/.height - sticky__items.getBoundingClientRect().height,
+    sticky__position = 0,
+    scrolled = 0,
+    scroll__top = 0
 window.addEventListener('scroll', (e) => {
-    document.body.style.cssText = `--scrollTop: ${this.scrollY}px`
-    let sticky__position = sticky__items.getBoundingClientRect().top
+    /*Параллакс эффект*/
+    scroll__top = this.scrollY
+    if (scroll__top > window.screen.height) { 
+        document.body.style.cssText = `--scrollTop: ${0}px`
+     } else {
+        document.body.style.cssText = `--scrollTop: ${scroll__top}px`
+    }
+    sticky__position = sticky__items.getBoundingClientRect().top
     if (sticky__position > 1) { return } else {
-        let scrolled = sticky__inner.getBoundingClientRect().top
+        scrolled = sticky__inner.getBoundingClientRect().top
         sticky__items.scrollLeft = (scroll__width / vertical__scroll__height) * (-scrolled) / 0.90
     }
 })
